9f8678cf1832e4d8a95cf0b6d1a1cab9ce8b3c46
canto9112 <cantoweb112@gmail.com>
2021-01-11 17:54:46 +0300

    Вынес метод response_json() в отдельную переменную

--- main.py
+++ main.py
@@ -13,11 +13,12 @@ def get_check_result(url, dvm_token, tg_token, chat_id):
         response = requests.get(url, headers=headers, params=params, timeout=95)
         response.raise_for_status()
         bot = telegram.Bot(token=tg_token)
-        if response.json()['status'] == 'found':
-            lesson_title = response.json()['new_attempts'][0]['lesson_title']
-            lesson_url = response.json()['new_attempts'][0]['lesson_url']
-            negative_result = response.json()['new_attempts'][0]['is_negative']
-            timestamp = response.json()['last_attempt_timestamp']
+        response_json = response.json()
+        if response_json['status'] == 'found':
+            lesson_title = response_json['new_attempts'][0]['lesson_title']
+            lesson_url = response_json['new_attempts'][0]['lesson_url']
+            negative_result = response_json['new_attempts'][0]['is_negative']
+            timestamp = response_json['last_attempt_timestamp']
             if negative_result:
                 bot.send_message(chat_id=chat_id, text='У вас проверили работу - {}\n'
                                                        'https://dvmn.org{}\n'
@@ -27,7 +28,7 @@ def get_check_result(url, dvm_token, tg_token, chat_id):
                                                        'https://dvmn.org{}\n'
                                                        'Работу приняли!'.format(lesson_title, lesson_url))
         elif response.json()['status'] == 'timeout':
-            timestamp = response.json()['timestamp_to_request']
+            timestamp = response_json['timestamp_to_request']
 
 
 if __name__ == '__main__':
